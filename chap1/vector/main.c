648e87bb72a90bda006b241599ab558d0f07df54
Jianwei Han <hanjianwei@gmail.com>
2018-03-22 00:16:35 +0800

    Add error checking

--- chap1/vector/main.c
+++ chap1/vector/main.c
@@ -1,82 +1,107 @@
 #include "vector.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  struct vector v, a, b;
+void exit_if_error(enum cc_stat st) {
+  if (st == CC_OK) {
+    return;
+  }
+
+  switch (st) {
+  case CC_ERR_ALLOC:
+    printf("Memory allocation error\n");
+    break;
+  case CC_ERR_INVALID_CAPACITY:
+    printf("Invalide capacity\n");
+    break;
+  case CC_ERR_INVALID_RANGE:
+    printf("Invalide range\n");
+    break;
+  case CC_ERR_OUT_OF_RANGE:
+    printf("Out of range\n");
+    break;
+  default:
+    printf("Unknown error\n");
+    break;
+  }
+
+  exit(1);
+}
+
+struct vector *generate_vector(int from, int to) {
+  int i;
+  enum cc_stat err;
+  int step = (from < to ? 1 : -1);
+  struct vector *v = (struct vector *)malloc(sizeof(struct vector));
+
+  if (v == NULL) {
+    exit_if_error(CC_ERR_ALLOC);
+  }
+
+  err = vector_init(v, 10);
+  exit_if_error(err);
+
+  for (i = from; i != to; i += step) {
+    err = vector_push_back(v, i);
+    exit_if_error(err);
+  }
+
+  return v;
+}
+
+void free_vector(struct vector *v) {
+  vector_destory(v);
+  free(v);
+}
+
+/* Test swap */
+void test_swap() {
+  struct vector *a = generate_vector(0, 5);
+  struct vector *b = generate_vector(2, 7);
+
+  vector_print(a);
+  vector_print(b);
+
+  free_vector(a);
+  free_vector(b);
+}
+
+void test_shrink() {
+  struct vector *a = generate_vector(0, 5);
+
+  printf("capacity of a: %d\n", a->capacity);
+  vector_shrink_to_fit(a);
+  printf("shrink capacity of a: %d\n", a->capacity);
+
+  free_vector(a);
+}
+
+void test_reverse() {
+  struct vector *a = generate_vector(0, 5);
+
+  vector_reverse(a);
+  vector_print(a);
+
+  free_vector(a);
+}
+
+void test_at() {
+  struct vector *a = generate_vector(0, 5);
   int i;
 
-  vector_init(&v, 10);
-  vector_init(&a, 10);
-  vector_init(&b, 10);
-
-  for (i = 0; i < 5; i++) {
-    vector_push_back(&a, i);
-  }
-
-  for (i = 2; i < 7; i++) {
-    vector_push_back(&b, i);
-  }
-
-  vector_print(&a);
-  vector_print(&b);
-
-  vector_swap(&a, &b);
-
-  vector_print(&a);
-  vector_print(&b);
-
-  printf("capacity of a: %d\n", a.capacity);
-  vector_shrink_to_fit(&a);
-  printf("shrink capacity of a: %d\n", a.capacity);
-
-  vector_reverse(&a);
-  vector_print(&a);
-
   for (i = -10; i < 10; i++) {
-    printf("%d ", vector_at(&a, i));
+    printf("%d ", vector_at(a, i));
   }
 
+  free_vector(a);
+}
 
-  /* vector_intersection(&v, &a, &b); */
+int main() {
 
-  /* for (i = 0; i < 10; i++) { */
-  /*   vector_push_front(&v, i); */
-  /* } */
-  /* vector_print(&v); */
-
-  /* for (i = 0 ; i < 5; i++) { */
-  /*   vector_pop_back(&v); */
-  /* } */
-
-  /* vector_print(&v); */
-
-  /* for (i = 0; i < 5; i++) { */
-  /*   vector_push_back(&v, i); */
-  /* } */
-  /* for (i = 0; i < 5; i++) { */
-  /*   vector_push_back(&v, i); */
-  /* } */
-  /* for (i = 0; i < 5; i++) { */
-  /*   vector_push_back(&v, i); */
-  /* } */
-
-  /* vector_print(&v); */
-
-  /* printf("Find all zero positions\n"); */
-  /* i = vector_find_first(&v, 0); */
-  /* printf("%d\n", i); */
-
-  /* while ((i = vector_find_next(&v, 0, i+1)) != -1) { */
-  /*   printf("%d\n", i); */
-  /* } */
-
-  /* vector_delete_value(&v, 0); */
-
-  /* vector_print(&v); */
-
-  vector_destory(&v);
-  vector_destory(&a);
-  vector_destory(&b);
+  test_swap();
+  test_reverse();
+  test_shrink();
+  test_at();
 
   return 0;
 }
