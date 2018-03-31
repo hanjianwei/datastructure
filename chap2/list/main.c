5ece99eab06e5205ae0d7c22e4bfa1ecfddb45b4
Jianwei Han <hanjianwei@gmail.com>
2018-03-31 10:51:54 +0800

    Merge two lists

--- chap2/list/main.c
+++ chap2/list/main.c
@@ -1,41 +1,79 @@
 #include "list.h"
+#include "sorted_list.h"
 
 #include <stdio.h>
 
 void print_list(struct List *list) {
   struct ListNode *p = list->head;
 
+  printf("[");
+
   while (p) {
-    printf("%d ", p->data);
+    printf("%d", p->data);
     p = p->next;
+    if (p) {
+      printf(", ");
+    }
   }
 
-  printf("\n");
+  printf("]\n");
 }
 
+void fill_list(struct List *list, int from, int to, int step) {
+  for (int i = from; i < to; i += step) {
+    list_push_back(list, i);
+  }
+}
 
-int main() {
+void test_list() {
   struct List s;
-  int i;
-
   list_init(&s);
 
-  for (i = 0; i < 5; i++) {
-    list_push_back(&s, i);
-  }
-
+  printf("Insert 0, 1, 2, 3, 4\n");
+  fill_list(&s, 0, 5, 1);
   print_list(&s);
 
-  printf("size: %d\n", s.size);
-
-
+  printf("Pop front\n");
   list_pop_front(&s);
-
   print_list(&s);
 
   printf("size: %d\n", s.size);
 
   list_destroy(&s);
+}
+
+void test_merge_two_lists() {
+  struct List s1, s2, dst;
+
+  list_init(&s1);
+  list_init(&s2);
+  list_init(&dst);
+
+  fill_list(&s1, 0, 5, 1);
+  fill_list(&s2, 2, 13, 2);
+
+  printf("s1: ");
+  print_list(&s1);
+  printf("s2: ");
+  print_list(&s2);
+
+  printf("Merge s1 and s2\n");
+  merge_sorted_lists(&dst, &s1, &s2);
+  printf("s1: ");
+  print_list(&s1);
+  printf("s2: ");
+  print_list(&s2);
+  printf("dst: ");
+  print_list(&dst);
+
+  list_destroy(&s1);
+  list_destroy(&s2);
+  list_destroy(&dst);
+}
+
+int main() {
+  /* test_list(); */
+  test_merge_two_lists();
 
   return 0;
 }
--- chap2/list/sorted_list.c
+++ chap2/list/sorted_list.c
@@ -1,8 +1,56 @@
 #include "sorted_list.h"
+#include <stdio.h>
+
+// Move node from head of src to tail of dst
+void move_node(struct List *src, struct List *dst) {
+  if (src->size == 0) {
+    return;
+  }
+
+  if (dst->size == 0) {
+    dst->head = src->head;
+  } else {
+    dst->tail->next = src->head;
+  }
+
+  dst->tail = src->head;
+  src->head = src->head->next;
+  dst->tail->next = NULL;
+  src->size--;
+  dst->size++;
+}
+
+void move_list(struct List *src, struct List *dst) {
+  if (dst->size == 0) {
+    dst->head = src->head;
+  } else {
+    dst->tail->next = src->head;
+  }
+  dst->tail = src->tail;
+  dst->size += src->size;
+
+  src->head = NULL;
+  src->tail = NULL;
+  src->size = 0;
+}
 
 // TODO:
 enum Status merge_sorted_lists(struct List *dst, struct List *src1,
                                struct List *src2) {
+  while (src1->head != NULL && src2->head != NULL) {
+    if (src1->head->data < src2->head->data) {
+      move_node(src1, dst);
+    } else {
+      move_node(src2, dst);
+    }
+  }
+
+  if (src1->head != NULL) {
+    move_list(src1, dst);
+  } else {
+    move_list(src2, dst);
+  }
+
   return STATUS_OK;
 }
 
