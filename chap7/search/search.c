3a861e297884059c62a19fc37cbcc4d06fffb345
Jianwei Han <hanjianwei@gmail.com>
2018-06-01 22:24:21 +0800

    Add chaper 7

---
+++ chap7/search/search.c
@@ -0,0 +1,21 @@
+#include "search.h"
+
+// TODO:
+// 使用comp对students进行排序
+void sort(struct Vector *students, StudentComp comp) {
+
+}
+
+// TODO:
+// 在students中查找stu，使用comp进行比较
+// 返回查找元素的下标，如果找不到返回-1
+int search(struct Vector *students, struct Student *stu, StudentComp comp) {
+  return 0;
+}
+
+// TODO:
+// 在students中用二分查找stu，使用comp进行比较
+// 返回查找元素的下标，如果找不到返回-1
+int bin_search(struct Vector *students, struct Student *stu, StudentComp comp) {
+  return 0;
+}
